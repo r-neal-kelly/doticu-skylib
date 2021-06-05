18a61b7e7558f4f7fc131decaf8c89c190fc4a6b
r-neal-kelly <r.neal.kelly@gmail.com>
2021-06-05 04:18:18 -0400

    whoops, forgot to hook up Delete_File to the actual OS_t.

--- Source/Plugins/doticu_skylib/src/doticu_skylib/os.cpp
+++ Source/Plugins/doticu_skylib/src/doticu_skylib/os.cpp
@@ -123,12 +123,12 @@ namespace doticu_skylib {
         return documents_path;
     }
 
-    static void Delete_File(const std::wstring& file_name)
+    void OS_t::Delete_File(const std::wstring& file_name)
     {
         DeleteFileW(file_name.c_str());
     }
 
-    static void Delete_File(std::wstring&& file_name)
+    void OS_t::Delete_File(std::wstring&& file_name)
     {
         Delete_File(file_name);
     }
